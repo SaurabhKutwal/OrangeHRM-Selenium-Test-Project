7df1c6ba92af1aaff2d95ffecea351477666fe93
Saurabh Kutwal <kutwalsaurabh19@gmail.com>
2024-12-19 22:16:24 +0530

    Login page test begins

--- src/test/java/TestCases/T_LoginTest.java
+++ src/test/java/TestCases/T_LoginTest.java
@@ -1,4 +1,14 @@
 package TestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class T_LoginTest {
+
+    @Test
+    public void test(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
 }
